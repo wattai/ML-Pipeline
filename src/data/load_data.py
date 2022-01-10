f1af960f5498bf5cb8eafeaf3fa44ca9f3ab79af
wattai <evamark.wattai@gmail.com>
2022-01-10 16:53:25 +0900

    Do until just before heroku deploy

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,6 +1,8 @@
-import yaml
 import argparse
-import pandas as pd 
+
+import pandas as pd
+import yaml
+
 
 def read_params(config_path):
     """
@@ -12,31 +14,34 @@ def read_params(config_path):
         config = yaml.safe_load(yaml_file)
     return config
 
-def load_data(data_path,model_var):
+
+def load_data(data_path, model_var):
     """
     load csv dataset from given path
-    input: csv path 
-    output:pandas dataframe 
+    input: csv path
+    output:pandas dataframe
     note: Only 6 variables are used in this model building stage for the simplicity.
     """
-    df = pd.read_csv(data_path, sep=",", encoding='utf-8')
-    df=df[model_var]
+    df = pd.read_csv(data_path, sep=",", encoding="utf-8")
+    df = df[model_var]
     return df
+
 
 def load_raw_data(config_path):
     """
-    load data from external location(data/external) to the raw folder(data/raw) with train and teting dataset 
-    input: config_path 
-    output: save train file in data/raw folder 
+    load data from external location(data/external) to the raw folder(data/raw) with train and teting dataset
+    input: config_path
+    output: save train file in data/raw folder
     """
-    config=read_params(config_path)
-    external_data_path=config["external_data_config"]["external_data_csv"]
-    raw_data_path=config["raw_data_config"]["raw_data_csv"]
-    model_var=config["raw_data_config"]["model_var"]
-    
-    df=load_data(external_data_path,model_var)
-    df.to_csv(raw_data_path,index=False)
-    
+    config = read_params(config_path)
+    external_data_path = config["external_data_config"]["external_data_csv"]
+    raw_data_path = config["raw_data_config"]["raw_data_csv"]
+    model_var = config["raw_data_config"]["model_var"]
+
+    df = load_data(external_data_path, model_var)
+    df.to_csv(raw_data_path, index=False)
+
+
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
--- src/models/production_model_selection.py
+++ src/models/production_model_selection.py
@@ -1,13 +1,15 @@
-import joblib
-import mlflow
 import argparse
 from pprint import pprint
-from train_model import read_params
+
+import joblib
+import mlflow
 from mlflow.tracking import MlflowClient
+from train_model import read_params
+
 
 def log_production_model(config_path):
     config = read_params(config_path)
-    mlflow_config = config["mlflow_config"] 
+    mlflow_config = config["mlflow_config"]
     model_name = mlflow_config["registered_model_name"]
     model_dir = config["model_dir"]
     remote_server_uri = mlflow_config["remote_server_uri"]
@@ -15,8 +17,10 @@ def log_production_model(config_path):
     mlflow.set_tracking_uri(remote_server_uri)
     runs = mlflow.search_runs(experiment_ids=1)
     max_accuracy = max(runs["metrics.accuracy"])
-    max_accuracy_run_id = list(runs[runs["metrics.accuracy"] == max_accuracy]["run_id"])[0]
-    
+    max_accuracy_run_id = list(
+        runs[runs["metrics.accuracy"] == max_accuracy]["run_id"]
+    )[0]
+
     client = MlflowClient()
     for mv in client.search_model_versions(f"name='{model_name}'"):
         mv = dict(mv)
@@ -26,22 +30,19 @@ def log_production_model(config_path):
             logged_model = mv["source"]
             pprint(mv, indent=4)
             client.transition_model_version_stage(
-                name=model_name,
-                version=current_version,
-                stage="Production"
+                name=model_name, version=current_version, stage="Production"
             )
         else:
             current_version = mv["version"]
             client.transition_model_version_stage(
-                name=model_name,
-                version=current_version,
-                stage="Staging"
-            )        
+                name=model_name, version=current_version, stage="Staging"
+            )
 
     loaded_model = mlflow.pyfunc.load_model(logged_model)
     joblib.dump(loaded_model, model_dir)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
