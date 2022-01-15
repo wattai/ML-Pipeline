cd8ebc33ef9a1f2f9d367ee650247f8aaff9a066
wattai <evamark.wattai@gmail.com>
2022-01-15 17:51:38 +0900

    Format with black

--- src/models/model_monitor.py
+++ src/models/model_monitor.py
@@ -1,8 +1,10 @@
-import yaml
 import argparse
+
 import pandas as pd
+import yaml
 from evidently.dashboard import Dashboard
-from evidently.tabs import DataDriftTab,CatTargetDriftTab
+from evidently.tabs import CatTargetDriftTab, DataDriftTab
+
 
 def read_params(config_path):
     """
@@ -14,25 +16,29 @@ def read_params(config_path):
         config = yaml.safe_load(yaml_file)
     return config
 
+
 def model_monitoring(config_path):
     config = read_params(config_path)
     train_data_path = config["raw_data_config"]["raw_data_csv"]
-    new_train_data_path=config["raw_data_config"]["new_train_data_csv"]
+    new_train_data_path = config["raw_data_config"]["new_train_data_csv"]
     target = config["raw_data_config"]["target"]
     monitor_dashboard_path = config["model_monitor"]["monitor_dashboard_html"]
     monitor_target = config["model_monitor"]["target_col_name"]
 
-    ref=pd.read_csv(train_data_path)
-    cur=pd.read_csv(new_train_data_path)
+    ref = pd.read_csv(train_data_path)
+    cur = pd.read_csv(new_train_data_path)
 
-    ref=ref.rename(columns ={target:monitor_target}, inplace = False)
-    cur=cur.rename(columns ={target:monitor_target}, inplace = False)
-    
-    data_and_target_drift_dashboard = Dashboard(tabs=[DataDriftTab(), CatTargetDriftTab()])
-    data_and_target_drift_dashboard.calculate(ref,cur, column_mapping = None)
+    ref = ref.rename(columns={target: monitor_target}, inplace=False)
+    cur = cur.rename(columns={target: monitor_target}, inplace=False)
+
+    data_and_target_drift_dashboard = Dashboard(
+        tabs=[DataDriftTab(), CatTargetDriftTab()]
+    )
+    data_and_target_drift_dashboard.calculate(ref, cur, column_mapping=None)
     data_and_target_drift_dashboard.save(monitor_dashboard_path)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
